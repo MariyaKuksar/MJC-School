bd50dc7e2f6027b22450a146d7173b6d836a4aa0
Mariya Kuksar <mariya.kuksar@gmail.com>
2021-08-30 15:55:30 +0300

    optimize import

--- gift-certificates-system/persistence/src/main/java/com/epam/esm/configuration/PersistenceConfiguration.java
+++ gift-certificates-system/persistence/src/main/java/com/epam/esm/configuration/PersistenceConfiguration.java
@@ -2,7 +2,10 @@ package com.epam.esm.configuration;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
--- gift-certificates-system/persistence/src/main/java/com/epam/esm/configuration/TestPersistenceConfiguration.java
+++ gift-certificates-system/persistence/src/main/java/com/epam/esm/configuration/TestPersistenceConfiguration.java
@@ -2,7 +2,6 @@ package com.epam.esm.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
